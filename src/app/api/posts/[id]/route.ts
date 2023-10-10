53b257ea34bf85d5720510098e036f07af7b49b2
Sahilbalhra <sahilbalhra0@gmail.com>
2023-10-10 23:36:47 +0530

    working completed

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -12,3 +12,13 @@ export const GET = async (request: NextRequest, { params }: any) => {
     return new NextResponse("Database Error", { status: 500 });
   }
 };
+export const DELETE = async (request: NextRequest, { params }: any) => {
+  const { id } = params;
+  try {
+    await connect();
+    await PostModel.findByIdAndDelete(id);
+    return new NextResponse("Post have been deleted", { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import styles from './navbar.module.css'
 import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
   {
@@ -37,6 +38,7 @@ const links = [
 ];
 
 const Navbar = () => {
+  const session = useSession()
   return (
     <div className={styles.container}>
       <Link href='/' className={styles.logo}>Blogs</Link>
@@ -47,7 +49,10 @@ const Navbar = () => {
             <Link href={link.url} key={link.id} className={styles.link}>{link.title}</Link>
           ))
         }
-        <button className={styles.logout} onClick={() => { console.log("logout") }}>Logout</button>
+        {
+          session.status === "authenticated" &&
+          <button className={styles.logout} onClick={() => signOut()}>Logout</button>
+        }
       </div>
     </div>
   )
